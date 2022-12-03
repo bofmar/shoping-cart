3421c60f708200775deebb39786cb54e3c7db47c
bofmar <bofmar@outlook.com>
2022-12-03 13:38:56 +0200

    Add lorem paragraph for item descriptions

--- src/data/items.js
+++ src/data/items.js
@@ -3,6 +3,8 @@ import Item from './Item';
 // LONG import the images
 import testImage from '../images/test-image.jpeg'; //for tests only TODO: Remove before final push
 
+const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mollis quam id diam finibus luctus. Curabitur hendrerit eget libero id placerat. In id vestibulum turpis. Mauris scelerisque sagittis felis, faucibus semper justo lobortis ut. Morbi tempus neque id mi elementum, ut mollis felis vestibulum. Duis pharetra sapien lacus, et ultrices dolor tempus sit amet. Donec quis hendrerit erat. Vivamus in sem lorem. Curabitur non est vitae justo condimentum aliquam. Proin suscipit, quam eu condimentum suscipit, tortor felis tristique enim, at consequat lorem ex et ipsum. Donec eget gravida lacus, sit amet semper diam. Phasellus ipsum lacus, aliquam ac orci non, pellentesque dignissim dolor. ';
+
 /* items object 
     form  -guitars[]
           -amps[]
@@ -12,7 +14,7 @@ import testImage from '../images/test-image.jpeg'; //for tests only TODO: Remove
 
 const items = {
   guitars: [
-    new Item('test guitar', 330.50, 'fender', testImage),
+    new Item('test guitar', 330.50, 'fender', description, testImage),
   ],
   amps: [
 
