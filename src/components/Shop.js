99ddd211658fe22558758810eab3575c46626b27
bofmar <bofmar@outlook.com>
2022-12-03 14:44:35 +0200

    Add sidebar to the shop

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 export default function Shop() {
   return (
-    <div className='product-area'>
-      <Outlet />
-    </div>
+    <>
+      <nav className='sidebar'>
+        <ul>
+          <li><NavLink to='guitars'>Guitars</NavLink></li>
+          <li><NavLink to='amps'>Amplifiers</NavLink></li>
+          <li><NavLink to='pedals'>Pedals/Effects</NavLink></li>
+          <li><NavLink to='accessories'>Accessories</NavLink></li>
+        </ul>
+      </nav>
+      <div className='product-area'>
+        <Outlet />
+      </div>
+    </>
   );
 }
