bddf7d6cd74c756124e8c5a1c41c3b147ed349ba
bofmar <bofmar@outlook.com>
2022-12-03 12:35:27 +0200

    Add route for incorect urls

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Shop from './components/Shop';
+import NoRoute from './components/NoRoute';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='shop' element={<Shop />} />
+      <Route path='*' element={<NoRoute />} />
     </Routes>
   );
 }
