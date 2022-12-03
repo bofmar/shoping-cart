a1e73f43bfe775749ad094b385e158cd90b0239c
bofmar <bofmar@outlook.com>
2022-12-03 13:42:06 +0200

    Create data for mocking

---
+++ src/tests/testData.js
@@ -0,0 +1,27 @@
+import testImage from '../images/test-image.jpeg';
+
+const testItem = {
+  id: 1234,
+  name: 'Guitar',
+  price: 200,
+  brand: 'testBrand',
+  deescription: 'description',
+  image: testImage
+};
+
+const testItems = {
+  guitars: [
+    testItem
+  ],
+  amps: [
+
+  ],
+  pedals: [
+
+  ],
+  accessories: [
+
+  ]
+};
+
+export { testItems, testItem }
