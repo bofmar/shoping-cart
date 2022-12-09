1ff7b84db23cf137aad44808b29dfb782dd37ca1
bofmar <bofmar@outlook.com>
2022-12-09 12:38:20 +0200

    Finalize about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,8 @@
 import React from "react";
-import placeholder from '../images/test-image.jpeg';
+import about1 from '../images/about1.jpeg';
+import about2 from '../images/about2.jpeg';
+import about3 from '../images/about3.jpeg';
+import about4 from '../images/about4.jpeg';
 
 export default function About() {
   return (
@@ -7,28 +10,28 @@ export default function About() {
       <section>
         <h1>Get Loud! The One Stop Guitar Shop for Everyone</h1>
         <div className='paragraph-with-image'>
-          <img src={placeholder} />
+          <img src={about1} />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
         </div>
       </section>
       <section>
         <h1>Our Rental Program</h1>
         <div className='paragraph-with-image'>
-          <img src={placeholder} />
+          <img src={about4} />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
         </div>
       </section>
       <section>
         <h1>Repairs</h1>
         <div className='paragraph-with-image'>
-          <img src={placeholder} />
+          <img src={about3} />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
         </div>
       </section>
       <section>
         <h1>Learning Center</h1>
         <div className='paragraph-with-image'>
-          <img src={placeholder} />
+          <img src={about2} />
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
         </div>
       </section>
