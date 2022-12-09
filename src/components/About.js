9905511614509ddb36f52858ba4a22fb5c1ef65e
bofmar <bofmar@outlook.com>
2022-12-09 12:18:03 +0200

    Add placeholders to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,37 @@
 import React from "react";
+import placeholder from '../images/test-image.jpeg';
 
 export default function About() {
   return (
-    <h1>About page</h1>
+    <div className='about-page'>
+      <section>
+        <h1>Get Loud! The One Stop Guitar Shop for Everyone</h1>
+        <div className='paragraph-with-image'>
+          <img src={placeholder} />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
+        </div>
+      </section>
+      <section>
+        <h1>Our Rental Program</h1>
+        <div className='paragraph-with-image'>
+          <img src={placeholder} />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
+        </div>
+      </section>
+      <section>
+        <h1>Repairs</h1>
+        <div className='paragraph-with-image'>
+          <img src={placeholder} />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
+        </div>
+      </section>
+      <section>
+        <h1>Learning Center</h1>
+        <div className='paragraph-with-image'>
+          <img src={placeholder} />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed auctor eros, id auctor sem. Donec accumsan nibh faucibus eros pharetra tincidunt ac vel arcu. In hac habitasse platea dictumst. Sed scelerisque pharetra arcu, sed scelerisque turpis blandit a. Vestibulum malesuada urna eu purus scelerisque, eu elementum est imperdiet. Donec quis ante neque. Sed id diam metus. Ut quis nulla pretium, ultrices sem vitae, sagittis nunc. Aliquam fermentum ante diam. </p>
+        </div>
+      </section>
+    </div>
   );
 }
